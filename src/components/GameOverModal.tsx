82eeaa1e38dbf70c28300c1961981a991556ea48
s.uchihori <s_uchihori@a-ssign.com>
2025-04-20 19:00:57 +0900

    fix lint

--- src/components/GameOverModal.tsx
+++ src/components/GameOverModal.tsx
@@ -28,6 +28,7 @@ const GameOverModal: React.FC<GameOverModalProps> = (
         <p>有効な手がなくなりました。</p>
         <p className="mb-4">ハイスコア: {highScore}</p>
         <button
+          type="button"
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           onClick={resetBoard}
         >
