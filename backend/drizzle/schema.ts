c69f894732d391a5b035a9d2010bae867670f87d
uchihori <show2305@gmail.com>
2025-05-06 00:45:42 +0900

    fix

--- backend/drizzle/schema.ts
+++ backend/drizzle/schema.ts
@@ -1,11 +1,9 @@
 import {
-  AnySQLiteColumn,
   integer,
   sqliteTable,
   text,
   uniqueIndex,
 } from "drizzle-orm/sqlite-core";
-import { sql } from "drizzle-orm";
 
 export const users = sqliteTable("users", {
   id: integer().primaryKey({ autoIncrement: true }).notNull(),
