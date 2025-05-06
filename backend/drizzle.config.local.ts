52056688249ea56d0a6f85373112577124662297
uchihori <show2305@gmail.com>
2025-05-07 02:19:15 +0900

    feat

--- backend/drizzle.config.local.ts
+++ backend/drizzle.config.local.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   schema: "./src/db/schema.ts",
   dialect: "sqlite",
   dbCredentials: {
-    url: ".wrangler/state/v3/d1/miniflare-D1DatabaseObject/b40b2d706a3d028d256a60103a0d9bd43054abdbe0c9e1d0e9bc53b697f9fb34.sqlite",
+    // biome-ignore lint/style/noNonNullAssertion:
+    url: process.env.LOCAL_DB_URL!,
   },
 });
